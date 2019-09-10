06fc9791dc2c8bf012ea30628bf63bd83c2ef539
Brandon Evans <brandon_n_evans@live.com>
2019-09-10 18:40:13 -0500

    add home, footer, header components

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <header class="row">
+        <nav class="navbar navbar-toggleable-md navbar-light">
+            <a href="/" class="navbar-brand">AUSTIN PIZZA</a>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="" class="nav-link">home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">menu</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
