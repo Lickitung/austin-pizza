d4ddb16706967e2ec6a84d95a9f8c35c6fc0d7f0
SIeep <brandon_n_evans@live.com>
2019-09-11 09:24:14 -0500

    add final components

---
+++ src/components/Contact.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="card">
+        <div class="card-block">
+            <h4 class="card-title">Contact</h4>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">
+                <address>
+                    <strong>Austin Pizza</strong><br>
+                    Address Line 1 <br>
+                    Address Line 2 <br>
+                    Phone: (123) 456-7890
+                </address>
+                <address>
+                    <strong>Email:</strong>
+                    <p>contact@austinpizza.com</p>
+                </address>
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Delivery.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="card">
+        <div class="card-block">
+            <h4 class="card-title">Contact</h4>
+            <p class="card-text">
+                Some example text.
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
